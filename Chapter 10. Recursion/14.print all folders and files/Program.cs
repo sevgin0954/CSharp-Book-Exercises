bc2f571123bf95ca32cb6d55f06409bf9e461d37
sevgin0954 <skateboardista_0954@abv.bg>
2017-10-03 19:06:50 +0300

    add program 14 in chapter 10

---
+++ Chapter 10. Recursion/14.print all folders and files/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        string startDirectory = Console.ReadLine();
+        PrintAllFiles(startDirectory);
+    }
+
+    static void PrintAllFiles(string startDirectory)
+    {
+        try
+        {
+            string[] folders = Directory.GetDirectories(startDirectory);
+            foreach (string folder in folders)
+            {
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(folder);
+                PrintAllFiles(folder);
+            }
+
+            string[] files = Directory.GetFiles(startDirectory);
+            foreach (string file in files)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine(file);
+            }
+        }
+        catch (UnauthorizedAccessException)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("ACCESS DENIED");
+        }
+    }
+}
